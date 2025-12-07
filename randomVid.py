3dc1a732943cadf783c786d23a9692669f03ad4e
Jule <jule@braun.cat>
2025-12-07 15:38:10 +0100

    Fullscreen/Caching

--- randomVid.py
+++ randomVid.py
@@ -4,15 +4,17 @@ import time
 import vlc
 from moviepy import VideoFileClip
 
-dir = '/home/jule/Coding/Python/Play-random-video/vids'
+dir = 'folder path'
 
 vids = os.listdir(dir)
 random.shuffle(vids)
 media_player = vlc.MediaPlayer()
+media_player.set_fullscreen(True)
 for vid in vids:
     path = os.path.join(dir, vid)
     duration = VideoFileClip(path).duration
-    media = vlc.Media(path)
+    duration = 2
+    media = vlc.Media(path, 'file-caching=3000')
     media_player.set_media(media)
     media_player.play()
     time.sleep(duration)
