674b97b3d091ce0a5d89d30f1f6d00308388f444
Jule <jule@braun.cat>
2025-12-07 14:57:48 +0100

    VLC improvements

---
+++ README.md
@@ -0,0 +1,5 @@
+## Install
+
+```bash
+pip install python-vlc
+pip install moviepy
--- randomVid.py
+++ randomVid.py
@@ -1,11 +1,18 @@
 import os
 import random
-import subprocess
+import time
+import vlc
+from moviepy import VideoFileClip
 
-dir = 'folder path'
+dir = '/home/jule/Coding/Python/Play-random-video/vids'
 
 vids = os.listdir(dir)
 random.shuffle(vids)
+media_player = vlc.MediaPlayer()
 for vid in vids:
     path = os.path.join(dir, vid)
-    subprocess.run(['vlc', '--play-and-exit', path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    duration = VideoFileClip(path).duration
+    media = vlc.Media(path)
+    media_player.set_media(media)
+    media_player.play()
+    time.sleep(duration)
